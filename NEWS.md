3d0c132ba2e64fdc5a85f8541afdc166fac65457
Thomas Ferte <thomas.ferte@u-bordeaux.fr>
2023-09-21 20:40:02 +0000

    version 1.0.3

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# PheVis 1.0.3
+
+Patch
+
+- Comply with new PKGNAME-package alias policy
+
 # version 1.0.2
 
 Minor:
---
+++ R/PheVis-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import Rcpp dplyr glmnet lme4 zoo purrr tidyr viridis knitr ggplot2
+#' @importFrom randomForest randomForest
+#' @importFrom stats as.formula predict quantile sd coef
+#' @importFrom Rcpp evalCpp
+#' @useDynLib PheVis
+## usethis namespace: end
+NULL
